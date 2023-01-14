60ddb89213fe46952f33670aebc9d1d8b242b3a7
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2023-01-13 19:57:31 -0500

    Updated tb_seven_segment_driver.v

--- Stopwatch/seven_segment_driver.v
+++ Stopwatch/seven_segment_driver.v
@@ -85,17 +85,17 @@ module seven_segment_driver(
     always @(*)
     begin
         case(LED_BCD)
-        4'b0000: LED_out = 7'b0000001; // "0"     
-        4'b0001: LED_out = 7'b1001111; // "1" 
-        4'b0010: LED_out = 7'b0010010; // "2" 
-        4'b0011: LED_out = 7'b0000110; // "3" 
-        4'b0100: LED_out = 7'b1001100; // "4" 
-        4'b0101: LED_out = 7'b0100100; // "5" 
-        4'b0110: LED_out = 7'b0100000; // "6" 
-        4'b0111: LED_out = 7'b0001111; // "7" 
-        4'b1000: LED_out = 7'b0000000; // "8"     
-        4'b1001: LED_out = 7'b0000100; // "9" 
-        default: LED_out = 7'b0000001; // "0"
+        4'b0000: display_out = 7'b0000001; // "0"     
+        4'b0001: display_out = 7'b1001111; // "1" 
+        4'b0010: display_out = 7'b0010010; // "2" 
+        4'b0011: display_out = 7'b0000110; // "3" 
+        4'b0100: display_out = 7'b1001100; // "4" 
+        4'b0101: display_out = 7'b0100100; // "5" 
+        4'b0110: display_out = 7'b0100000; // "6" 
+        4'b0111: display_out = 7'b0001111; // "7" 
+        4'b1000: display_out = 7'b0000000; // "8"     
+        4'b1001: display_out = 7'b0000100; // "9" 
+        default: display_out = 7'b0000001; // "0"
         endcase
     end
 
--- Stopwatch/tb_seven_segment_driver.v
+++ Stopwatch/tb_seven_segment_driver.v
@@ -15,5 +15,52 @@ module tb_seven_segment_driver;
     wire [3:0] anode_signals;
     wire [6:0] display_out;
 
-    
+    // Design under test
+    seven_segment_driver dut (
+        .clock(clock),
+        .reset(reset),
+        .minutes(minutes),
+        .seconds(seconds),
+        .anode_signals(anode_signals),
+        .display_out(display_out)
+    );
+
+    // Generate 50Mhz clock signal
+    always #10 clock = ~clock;
+
+    // Generate stimulus
+    initial begin
+        // Initialize inputs
+        reset <= 1;
+        clock <= 0;
+        minutes <= 4'b0000;
+        seconds <= 4'b0000;
+
+        // Start the 7 segment driver
+        #10 reset <= 0;
+
+        // Wait for global reset
+        #20
+
+        // Generate stimulus
+        int test_number = 0;
+
+        // Build array of correct outputs for decimal 1-9
+        reg [6:0] test_cases [0:10];
+        test_cases[0] = 7'b0000001; // 0
+        test_cases[1] = 7'b1001111; // 1
+        test_cases[2] = 7'b0010010; // 2
+        test_cases[3] = 7'b0000110; // 3
+        test_cases[4] = 7'b1001100; // 4
+        test_cases[5] = 7'b0100100; // 5
+        test_cases[6] = 7'b0100000; // 6
+        test_cases[7] = 7'b0001111; // 7
+        test_cases[8] = 7'b0000000; // 8
+        test_cases[9] = 7'b0000100; // 9
+
+        // Test the seconds input
+
+        // Test the minutes input
+
+    end
 endmodule
