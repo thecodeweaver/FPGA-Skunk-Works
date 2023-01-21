17325cbfa7f1090b8fddb901afe959ba3fbe20b3
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2023-01-21 16:58:28 -0500

    Update tb_seven_segment_driver.v

--- Stopwatch/tb_seven_segment_driver.v
+++ Stopwatch/tb_seven_segment_driver.v
@@ -28,6 +28,22 @@ module tb_seven_segment_driver;
     // Generate 50Mhz clock signal
     always #10 clock = ~clock;
 
+    // Build array of correct outputs for decimal 1-9
+    reg [6:0] test_cases [0:10];
+    test_cases[0] = 7'b0000001; // 0
+    test_cases[1] = 7'b1001111; // 1
+    test_cases[2] = 7'b0010010; // 2
+    test_cases[3] = 7'b0000110; // 3
+    test_cases[4] = 7'b1001100; // 4
+    test_cases[5] = 7'b0100100; // 5
+    test_cases[6] = 7'b0100000; // 6
+    test_cases[7] = 7'b0001111; // 7
+    test_cases[8] = 7'b0000000; // 8
+    test_cases[9] = 7'b0000100; // 9
+
+    // Blocks to check the value of display_out depending on which digit is enabled
+    
+
     // Generate stimulus
     initial begin
         // Initialize inputs
@@ -43,24 +59,22 @@ module tb_seven_segment_driver;
         #20
 
         // Generate stimulus
-        int test_number = 0;
-
-        // Build array of correct outputs for decimal 1-9
-        reg [6:0] test_cases [0:10];
-        test_cases[0] = 7'b0000001; // 0
-        test_cases[1] = 7'b1001111; // 1
-        test_cases[2] = 7'b0010010; // 2
-        test_cases[3] = 7'b0000110; // 3
-        test_cases[4] = 7'b1001100; // 4
-        test_cases[5] = 7'b0100100; // 5
-        test_cases[6] = 7'b0100000; // 6
-        test_cases[7] = 7'b0001111; // 7
-        test_cases[8] = 7'b0000000; // 8
-        test_cases[9] = 7'b0000100; // 9
 
         // Test the seconds input
+        reg [5:0] test_number = 6'd0;
+        repeat(60)
+        begin
+            seconds <= test_number;
+            #5; // Propagation delay
+            test_number <= test_number + 1;
+        end
 
         // Test the minutes input
-
+        test_number = 6'd0;
+        begin
+            seconds <= test_number;
+            #5; // Propagation delay
+            test_number <= test_number + 1;
+        end
     end
 endmodule
