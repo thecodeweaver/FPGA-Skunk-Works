c42d24c8ee16424b9851fc497332475d9aa96123
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2023-01-25 17:30:59 -0500

    Got 7 segment driver testbench to compile

--- Stopwatch/seven_segment_driver.v
+++ Stopwatch/seven_segment_driver.v
@@ -23,8 +23,8 @@ module seven_segment_driver(
     input reset,
     input [5:0] minutes,
     input [5:0] seconds,
-    output [3:0] anode_signals,
-    output [6:0] display_out
+    output reg [3:0] anode_signals,
+    output reg [6:0] display_out
 );
 
     reg [3:0] LED_BCD; // Binary signal which indicates which cathodes to toggle to enable LEDs on display
@@ -54,28 +54,28 @@ module seven_segment_driver(
                 begin
                     // Display the first digit of the minutes number
                     // activate LED1 and Deactivate LED2, LED3, LED4
-                    Anode_Activate = 4'b0111; 
+                    anode_signals = 4'b0111; 
                     LED_BCD = minutes / 10;
                 end
             2'b01:
                 begin
                     // Display the second digit of the minutes number
                     // activate LED2 and Deactivate LED1, LED3, LED4
-                    Anode_Activate = 4'b1011;
+                    anode_signals = 4'b1011;
                     LED_BCD = minutes % 10;
                 end
             2'b10:
                 begin
                     // Display the first digit of the seconds number
                     // activate LED3 and Deactivate LED2, LED1, LED4
-                    Anode_Activate = 4'b1101;
+                    anode_signals = 4'b1101;
                     LED_BCD = seconds / 10;
                 end
             2'b11: 
                 begin
                     // Display the second digit of the seconds number
                     // activate LED4 and Deactivate LED2, LED3, LED1
-                    Anode_Activate = 4'b1110;
+                    anode_signals = 4'b1110;
                     LED_BCD = minutes % 10;
                 end
         endcase
@@ -85,17 +85,17 @@ module seven_segment_driver(
     always @(*)
     begin
         case(LED_BCD)
-        4'b0000: display_out = 7'b0000001; // "0"     
-        4'b0001: display_out = 7'b1001111; // "1" 
-        4'b0010: display_out = 7'b0010010; // "2" 
-        4'b0011: display_out = 7'b0000110; // "3" 
-        4'b0100: display_out = 7'b1001100; // "4" 
-        4'b0101: display_out = 7'b0100100; // "5" 
-        4'b0110: display_out = 7'b0100000; // "6" 
-        4'b0111: display_out = 7'b0001111; // "7" 
-        4'b1000: display_out = 7'b0000000; // "8"     
-        4'b1001: display_out = 7'b0000100; // "9" 
-        default: display_out = 7'b0000001; // "0"
+        4'b0000: display_out <= 7'b0000001; // "0"     
+        4'b0001: display_out <= 7'b1001111; // "1" 
+        4'b0010: display_out <= 7'b0010010; // "2" 
+        4'b0011: display_out <= 7'b0000110; // "3" 
+        4'b0100: display_out <= 7'b1001100; // "4" 
+        4'b0101: display_out <= 7'b0100100; // "5" 
+        4'b0110: display_out <= 7'b0100000; // "6" 
+        4'b0111: display_out <= 7'b0001111; // "7" 
+        4'b1000: display_out <= 7'b0000000; // "8"     
+        4'b1001: display_out <= 7'b0000100; // "9" 
+        default: display_out <= 7'b0000001; // "0"
         endcase
     end
 
--- Stopwatch/tb_seven_segment_driver.v
+++ Stopwatch/tb_seven_segment_driver.v
@@ -14,6 +14,9 @@ Test procedure
 
 *** MINUTES ***
 Same procedure for seconds but input range is 0-99
+
+*** NOTES ***
+Apparently you can only assign values inside initial begin blocks in testbenches??
 */
 
 module tb_seven_segment_driver;
@@ -38,67 +41,59 @@ module tb_seven_segment_driver;
         .display_out(display_out)
     );
 
+	// Build array of correct outputs for decimal 1-9
+	// Each bit corresponds to a different cathode on the 7 segment display (1 = OFF, 0 = ON)
+    reg [0:6] test_cases [9:0];
+    
+
     // Generate 50Mhz clock signal
     always #10 clock = ~clock;
 
-    // Build array of correct outputs for decimal 1-9
-    reg [6:0] test_cases [0:10];
-    test_cases[0] = 7'b0000001; // 0
-    test_cases[1] = 7'b1001111; // 1
-    test_cases[2] = 7'b0010010; // 2
-    test_cases[3] = 7'b0000110; // 3
-    test_cases[4] = 7'b1001100; // 4
-    test_cases[5] = 7'b0100100; // 5
-    test_cases[6] = 7'b0100000; // 6
-    test_cases[7] = 7'b0001111; // 7
-    test_cases[8] = 7'b0000000; // 8
-    test_cases[9] = 7'b0000100; // 9
-
     // Block to check the value of display_out depending on which digit is enabled
-	forever
-	begin
-		case (anode_signals)
-			// 10's place of the seconds input
-			4'b1101:
-				begin
-					if (display_out != test_cases[(seconds / 10)])
+	initial begin
+		forever begin
+			case (anode_signals)
+				// 10's place of the seconds input
+				4'b1101:
 					begin
-						$display("10's place of the seconds input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(seconds / 10)]);
+						if (display_out != test_cases[(seconds / 10)]) begin
+							$display("10's place of the seconds input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(seconds / 10)]);
+						end
 					end
-				end
+					
+				// 1's place of the seconds input
+				4'b1110:
+					begin
+						if (display_out != test_cases[(seconds % 10)]) begin
+							$display("10's place of the seconds input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(seconds % 10)]);
+						end
+					end
+					
+				// 10's place of minutes input
+				4'b0111:
+					begin
+						if (display_out != test_cases[(minutes / 10)]) begin
+							$display("10's place of the minutes input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(minutes / 10)]);
+						end
+					end
 				
-			// 1's place of the seconds input
-			4'b1110:
-				begin
-					if (display_out != test_cases[(seconds % 10)])
+				// 1's place of minutes input
+				4'b1011:
 					begin
-						$display("10's place of the seconds input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(seconds % 10)]);
-					end
-				end
-				
-			// 10's place of minutes input
-			4'b0111:
-				begin
-					if (display_out != test_cases[(minutes / 10)])
-					begin
-						$display("10's place of the minutes input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(minutes / 10)]);
-					end
-				end
-			
-			// 1's place of minutes input
-			4'b1011:
-				begin
-					if (display_out != test_cases[(minutes / 10)])
-					begin
-						$display("10's place of the minutes input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(minutes % 10)]);
-					end
-				end				
-		endcase
+						if (display_out != test_cases[(minutes / 10)])
+						begin
+							$display("10's place of the minutes input doesn't match expected value\tdisplay_out = %b expected: %b", display_out, test_cases[(minutes % 10)]);
+						end
+					end				
+			endcase
+		end
 	end
+	
+	// Number used to test the inputs
+	reg [5:0] test_number;
 
     // Generate stimulus
-    initial
-	begin
+    initial begin
         // Initialize inputs
         reset <= 1;
         clock <= 0;
@@ -110,13 +105,24 @@ module tb_seven_segment_driver;
 
         // Wait for global reset
         #20
+		
+		// Initialize test cases
+		test_cases[0] = 7'b0000001; // 0
+		test_cases[1] = 7'b1001111; // 1
+		test_cases[2] = 7'b0010010; // 2
+		test_cases[3] = 7'b0000110; // 3
+		test_cases[4] = 7'b1001100; // 4
+		test_cases[5] = 7'b0100100; // 5
+		test_cases[6] = 7'b0100000; // 6
+		test_cases[7] = 7'b0001111; // 7
+		test_cases[8] = 7'b0000000; // 8
+		test_cases[9] = 7'b0000100; // 9
 
         // Generate stimulus
 
         // Test the seconds input
-        reg [5:0] test_number = 6'd0;
-        repeat(60)
-        begin
+        test_number = 6'd0;
+        repeat(60) begin
             seconds <= test_number;
             #5; // Propagation delay
             test_number <= test_number + 1;
@@ -126,8 +132,7 @@ module tb_seven_segment_driver;
 
         // Test the minutes input
         test_number = 6'd0;
-		repeat(100)
-        begin
+		repeat(100) begin
             seconds <= test_number;
             #5; // Propagation delay
             test_number <= test_number + 1;
